36e54fe8723ed7a69cedfbecb212ce102b301c76
ANKIT KUMAR <88587998+codeonstorm@users.noreply.github.com>
2023-10-25 14:19:36 +0600

    Delete ems/public directory

--- ems/public/partitions/footer.php
+++
@@ -1,6 +0,0 @@
-    <!-- Footer -->
-    <footer class="footer">
-        <div class="container text-center">
-            <p>&copy; 2023 <?= strtolower(WEBSITE_NAME); ?> </p>
-        </div>
-    </footer>
